f53dc722b03d4f0bf7bb69f3d9b409f26d307ed9
rob <bsd@interia.pl>
2016-06-30 12:03:04 +0200

    stop

--- core/src/com/mygdx/game/GdxSmokuProject.java
+++ core/src/com/mygdx/game/GdxSmokuProject.java
@@ -102,6 +102,7 @@ public class GdxSmokuProject extends Game
         mainStage.addActor( floor );
 
         
+        
      
      Skin mSkin = new Skin(Gdx.files.internal("data/uiskin.json"));   
         
